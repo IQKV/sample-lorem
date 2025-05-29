6fe4dae3b29bc398b58052f6f8715f2b4dbda12e
Dima Denisenko <1253839+dimdnk@users.noreply.github.com>
2025-05-29 12:36:10 +0300

    chore: simplify package naming for microservice

---
+++ words-processing/src/test/java/com/iqkv/sample/lorem/processing/consumer/WordsProcessingReportTestConsumer.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2025 IQKV Foundation Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.iqkv.sample.lorem.processing.consumer;
+
+import com.iqkv.sample.lorem.processing.model.Report;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class WordsProcessingReportTestConsumer {
+
+  @KafkaListener(containerFactory = "processingReportKafkaListenerContainerFactory",
+                 topics = "${lorem.kafka.topics.words-processed.name}",
+                 groupId = "${spring.kafka.consumer.group-id}")
+  public void consume(ConsumerRecord<String, Report> consumerRecord) {
+    log.info("( {} ) Received report, key: {}, value: {}",
+        Thread.currentThread().getName(), consumerRecord.key(), consumerRecord.value());
+  }
+}
